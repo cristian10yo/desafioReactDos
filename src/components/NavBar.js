4851c2ca83ecb059e3341c2a38b1b64842626eac
cristian almaraz <cristian28_lp@hotmail.com>
2022-09-08 14:35:17 -0300

    segunda entrega, agregar nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar (){
+    return(
+    <div>
+<h1 className="titulo-nav-bar">FALSYCOMMERCE</h1>
+<ul>
+    <li>Inicio</li>
+    <li>Bebidas</li>
+    <li>Auriculares</li>
+    <li>Smart TV</li>
+</ul>
+
+    </div>
+    );
+}
+
+export default NavBar;
