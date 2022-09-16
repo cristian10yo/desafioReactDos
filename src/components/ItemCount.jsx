14c3adf40098eab6a4ea4309aea8e71f37ed8867
cristian almaraz <cristian28_lp@hotmail.com>
2022-09-16 00:49:51 -0300

    entrega boton contador

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -4,15 +4,13 @@ import { FaSwatchbook } from "react-icons/fa";
 function ItemCount (props){
     const [count,setCount] = React.useState(props.initial)
 
-    function handleAdd (){
-        //solo sumar si la cantidad no excede el stock
-        setCount (count+1)
+    function handleAdd() {
+        if (count < props.stock) {setCount(count + 1);}
+    }
+    function handleSubstract() {
+        if (count > 0) {setCount(count - 1);}
     }
 
-    function handleSubstract (){
-        //solo restar si el numero es 0
-        setCount (count-1)
-    }
 
     return(
 <div>
